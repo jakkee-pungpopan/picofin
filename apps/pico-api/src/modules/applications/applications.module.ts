35e8a2436672499f2c795003ac4ab00b1d73ad6d
jakkee-pungpopan <jakkee.pungpopan@gmail.com>
2026-06-30 00:14:43 +0700

    Update applications.module.ts

--- apps/pico-api/src/modules/applications/applications.module.ts
+++ apps/pico-api/src/modules/applications/applications.module.ts
@@ -1,5 +1,6 @@
+import { LoansModule } from '../loans/loans.module';
 import { Module } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
-@Module({ providers: [ApplicationsService], controllers: [ApplicationsController], exports: [ApplicationsService] })
+@Module({ imports: [LoansModule], providers: [ApplicationsService], controllers: [ApplicationsController], exports: [ApplicationsService] })
 export class ApplicationsModule {}
