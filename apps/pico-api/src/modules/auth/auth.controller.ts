b97e331066f7948d8d26e5b33189f92889e025e9
jakkee-pungpopan <jakkee.pungpopan@gmail.com>
2026-06-29 17:36:27 +0700

    Update auth.controller.ts

--- apps/pico-api/src/modules/auth/auth.controller.ts
+++ apps/pico-api/src/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, HttpCode } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterBorrowerDto } from './dto/auth.dto';
-@ApiTags('auth') @Controller('auth')
+@ApiTags('เข้าสู่ระบบ') @Controller('auth')
 export class AuthController {
   constructor(private auth: AuthService) {}
   @Post('login') @HttpCode(200) @ApiOperation({ summary: 'เข้าสู่ระบบ (ผู้ประกอบการ/พนักงาน/ลูกค้า)' })
