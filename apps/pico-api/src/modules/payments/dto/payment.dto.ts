f989e0930fe3df8f9b423bcf00b7337a007c7559
jakkee-pungpopan <jakkee.pungpopan@gmail.com>
2026-06-29 18:50:54 +0700

    Update payment.dto.ts

--- apps/pico-api/src/modules/payments/dto/payment.dto.ts
+++ apps/pico-api/src/modules/payments/dto/payment.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsPositive, IsOptional, IsString } from 'class-validator';
 export class CreatePaymentDto {
-  @ApiProperty({ example: 1066.19, description: 'จำนวนรับชำระ (บาท)' }) @IsNumber() @IsPositive() amount: number;
-  @ApiProperty({ required: false, example: 'CASH' }) @IsOptional() @IsString() method?: string;
-  @ApiProperty({ required: false }) @IsOptional() @IsString() note?: string;
+  @ApiProperty({ example: 1066.19, description: 'จำนวนเงินที่รับชำระ (บาท)' }) @IsNumber() @IsPositive() amount: number;
+  @ApiProperty({ required: false, example: 'CASH', description: 'ช่องทางชำระ เช่น CASH (เงินสด) หรือ TRANSFER (โอน)' }) @IsOptional() @IsString() method?: string;
+  @ApiProperty({ required: false, example: 'ชำระงวดที่ 1', description: 'หมายเหตุ' }) @IsOptional() @IsString() note?: string;
 }
